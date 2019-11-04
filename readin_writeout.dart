fa2fa833803af0db76493686340715413f1a6a8d
Yuwono Bangun Nagoro <morpheusgun@outlook.com>
2019-11-04 08:48:45 +0700

    add io sample and a little bit of 'if' condition

---
+++ readin_writeout.dart
@@ -0,0 +1,21 @@
+import 'dart:io';
+
+void main(){
+    String name;
+    int age;
+    stdout.writeln("Hello, Can I have your name?");
+    name = stdin.readLineSync();
+    stdout.writeln("And, How old are you?");
+    age = int.parse(stdin.readLineSync());
+    if (age < 0){
+        stdout.writeln("Your age is invalid!");
+    }else if (age >=0 && age < 10){
+        stdout.writeln("Hey $name, it is good day to play around. Let's play!");
+    }else if (age>=10 && age <20){
+        stdout.writeln("Hey $name, it is great to be a teenager, isn't it?");
+    }else if (age>=20 && age <30){
+        stdout.writeln("Hey $name, how's life so far? Do you enjoy your life?");
+    }else{
+        stdout.writeln("Hey $name, I just wanna let you know that YOU ARE OLD!");
+    }
+}
